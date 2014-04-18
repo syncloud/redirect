3403a0898d8580d47f6c22f7bd88f678b4afd225
Boris Rybalkin <ribalkin@gmail.com>
2014-04-18 01:13:24 +0100

    fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if [ "$LATEST_REV" == "" ]; then
   exit 1
 fi
 
-if [ ! -f ${REV_FILE} ]; then
+if [ -f ${REV_FILE} ]; then
   CURRENT_REV=$(<${REV_FILE})
   if [ "$CURRENT_REV" == "$LATEST_REV" ]; then
     echo "No changes since last check"
