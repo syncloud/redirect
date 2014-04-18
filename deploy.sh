2efa73794aa75f57ef1a3c8835e240802a141269
Boris Rybalkin <ribalkin@gmail.com>
2014-04-18 01:12:58 +0100

    fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,14 @@ if [ "$LATEST_REV" == "" ]; then
   exit 1
 fi
 
-if [ -f ${REV_FILE} ]; then
+if [ ! -f ${REV_FILE} ]; then
   CURRENT_REV=$(<${REV_FILE})
   if [ "$CURRENT_REV" == "$LATEST_REV" ]; then
     echo "No changes since last check"
     exit 1
   fi
 fi
+echo "$LATEST_REV" > ${REV_FILE}
 
 git pull
 cp ${REV_FILE} www/${REV_FILE}
