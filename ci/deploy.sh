257f06de281b501c4853d66e8b67359498548127
Boris Rybalkin <ribalkin@gmail.com>
2014-04-18 01:08:06 +0100

    test environment continuous deploy

---
+++ ci/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+GIT_URL=https://github.com/syncloud/redirect
+REV_FILE=.revision
+LATEST_REV=$(git ls-remote ${GIT_URL} refs/heads/master | cut -f1)
+if [ "$LATEST_REV" == "" ]; then
+  echo "Unable to get latest version"
+  exit 1
+fi
+
+if [ -f ${REV_FILE} ]; then
+  CURRENT_REV=$(<${REV_FILE})
+  if [ "$CURRENT_REV" == "$LATEST_REV" ]; then
+    echo "No changes since last check"
+    exit 1
+  fi
+fi
+
+git pull
+cp ${REV_FILE} www/${REV_FILE}
+cd www
+jekyll
