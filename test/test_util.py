b653b49f3f516a4328cdc7cceff0c339ca513b77
vsapronov <vladimir.sapronov@gmail.com>
2014-06-11 09:13:40 -0400

    Improved tests

--- test/test_util.py
+++ test/test_util.py
@@ -1,5 +1,5 @@
 import unittest
-from redirect.util import hash
+from redirect.util import hash, get_second_level_domain
 
 class TestHash(unittest.TestCase):
 
@@ -17,6 +17,27 @@ class TestHash(unittest.TestCase):
         h1 = hash('some string')
         h2 = hash('some other string')
         self.assertNotEqual(h1, h2)
+
+class TestGetSecondLevelDomain(unittest.TestCase):
+
+    def test_simple(self):
+        domain = 'syncloud.com'
+        url = 'http://second.syncloud.com'
+        second_level_domain_name = get_second_level_domain(url, domain)
+        self.assertEquals('second', second_level_domain_name)
+
+    def test_long_url(self):
+        domain = 'syncloud.com'
+        url = 'http://second.syncloud.com/some/really/long/url/with_param=value'
+        second_level_domain_name = get_second_level_domain(url, domain)
+        self.assertEquals('second', second_level_domain_name)
+
+    def test_port(self):
+        domain = 'syncloud.com'
+        url = 'http://second.syncloud.com:10001/param=value'
+        second_level_domain_name = get_second_level_domain(url, domain)
+        self.assertEquals('second', second_level_domain_name)
+
 
 if __name__ == '__main__':
     unittest.run()
