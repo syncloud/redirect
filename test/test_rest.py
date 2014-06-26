43ca986fab735a56f668899fd5441d1501fac09f
vsapronov <vladimir.sapronov@gmail.com>
2014-06-25 23:59:48 -0400

    Testing rest without server, coverage should jump up!

--- test/test_rest.py
+++ test/test_rest.py
@@ -1,20 +1,23 @@
+import redirect.rest
 import unittest
-import requests
+
 import json
-from urlparse import urljoin
+
+from redirect.util import create_token
 from fakesmtp import FakeSmtp
 from urlparse import urlparse
-from redirect.util import create_token
 
-class TestWebRest(unittest.TestCase):
+class FlaskrTestCase(unittest.TestCase):
 
-    base_url = r'http://127.0.0.1:5000'
+    def setUp(self):
+        redirect.rest.app.config['TESTING'] = True
+        self.app = redirect.rest.app.test_client()
 
-    def post(self, url, params):
-        return requests.post(urljoin(self.base_url, url), data=params)
+        self.smtp = FakeSmtp('outbox')
+        self.smtp.clear()
 
-    def get(self, url, parameters):
-        return requests.get(urljoin(self.base_url, url), params=parameters)
+    def tearDown(self):
+        pass
 
     def get_token(self, email):
         link_index = email.find('http://')
@@ -23,16 +26,11 @@ class TestWebRest(unittest.TestCase):
         token = parts.query.replace('token=', '')
         return token
 
-    def setUp(self):
-        self.smtp = FakeSmtp('outbox')
-        self.smtp.clear()
-
     def test_user_create_success(self):
         user_domain = create_token()
         email = user_domain+'@mail.com'
         params = {'user_domain': user_domain, 'email': email, 'password': 'pass123456'}
-        response = self.post('/user/create', params)
-        self.assertTrue(response.ok, 'Response was: '+str(response))
+        response = self.app.post('/user/create', data=params)
         self.assertEqual(200, response.status_code)
         self.assertFalse(self.smtp.empty())
 
@@ -40,34 +38,27 @@ class TestWebRest(unittest.TestCase):
         user_domain = create_token()
         email = user_domain+'@mail.com'
         create_params = {'user_domain': user_domain, 'email': email, 'password': 'pass123456'}
-        self.post('/user/create', create_params)
+        self.app.post('/user/create', data=create_params)
 
         self.assertFalse(self.smtp.empty())
         token = self.get_token(self.smtp.emails()[0])
 
-        response = {'token': token}
-        activate_response = self.get('/user/activate', response)
-        self.assertTrue(activate_response.ok, 'Response was: '+str(activate_response))
+        params = {'token': token}
+        activate_response = self.app.get('/user/activate', query_string=params)
         self.assertEqual(200, activate_response.status_code)
 
     def test_domain_update(self):
         user_domain = create_token()
         email = user_domain+'@mail.com'
-        self.post('/user/create', {'user_domain': user_domain, 'email': email, 'password': 'pass123456'})
+        self.app.post('/user/create', data={'user_domain': user_domain, 'email': email, 'password': 'pass123456'})
         activate_token = self.get_token(self.smtp.emails()[0])
-        self.get('/user/activate', {'token': activate_token})
+        self.app.get('/user/activate', query_string={'token': activate_token})
 
-        token_response = self.get('/user/get', {'email': email, 'password': 'pass123456'})
-        self.assertTrue(token_response.ok, 'Response with code: {0}, Content: {1}'.format(token_response.status_code, token_response.content))
+        token_response = self.app.get('/user/get', query_string={'email': email, 'password': 'pass123456'})
         self.assertEqual(200, token_response.status_code)
-        user_data = json.loads(token_response.content)
+        user_data = json.loads(token_response.data)
 
         update_token = user_data['update_token']
 
-        response = self.post('/domain/update', {'token': update_token, 'ip': '127.0.0.1', 'port': '10001'})
-        self.assertTrue(response.ok, 'Response was: '+str(response))
+        response = self.app.post('/domain/update', data={'token': update_token, 'ip': '127.0.0.1', 'port': '10001'})
         self.assertEqual(200, response.status_code)
-
-
-if __name__ == '__main__':
-    unittest.run()
