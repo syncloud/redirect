ed748943e217d81b89b02da1339524d1c4655cf3
vsapronov <vladimir.sapronov@gmail.com>
2015-01-02 05:30:35 -0500

    Migration of services to 004 is done

--- db/004.sql
+++ db/004.sql
@@ -5,5 +5,9 @@ insert into `domain` (`id`, `user_domain`, `ip`, `local_ip`, `update_token`, `us
 select `id`, `user_domain`, `ip`, '0.0.0.0' as `local_ip`, `update_token`, `user_id`, '00:00:00:00:00:00' as `device_mac_address`, 'Not assigned' as `device_name`, 'Not assigned' as `device_title`, `last_update`
 from redirect_backup.`domain`;
 
+insert into `service` (`id`, `name`, `protocol`, `type`, `url`, `port`, `local_port`, `domain_id`)
+select `id`, `name`, `protocol`, `type`, `url`, `port`, 0 as `local_port`, `domain_id`
+from redirect_backup.`service`;
+
 insert into `action`
 select * from redirect_backup.`action`;
