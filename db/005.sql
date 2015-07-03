a500db17fb0bbd95901b6092ed94af562b462efa
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-03 16:16:31 -0400

    Fixing migration sql script

--- db/005.sql
+++ db/005.sql
@@ -3,14 +3,11 @@ select `id`, `email`, `password_hash`, `active`, `update_token`, 0 as `unsubscri
 from redirect_backup.`user`;
 
 insert into `domain`
-select *
-from redirect_backup.`domain`;
-
-insert into `domain`
 (`id`,
 `user_domain`,
 `ip`,
 `local_ip`,
+`map_local_address`,
 `update_token`,
 `user_id`,
 `device_mac_address`,
