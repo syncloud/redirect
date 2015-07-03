f394dbb57329dbd7f41b238315634da5a3446baa
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-02 21:25:41 -0400

    Fixing migrate script

--- db/005.sql
+++ db/005.sql
@@ -1,12 +1,13 @@
-insert into `user`
-select * from redirect_backup.`user`;
+insert into `user` (`id`, `email`, `password_hash`, `active`, `update_token`, `unsubscribed`, `timestamp`)
+select `id`, `email`, `password_hash`, `active`, `update_token`, 0 as `unsubscribed`, `timestamp`
+from redirect_backup.`user`;
 
-insert into `domain` (`id`, `user_domain`, `ip`, `local_ip`, `update_token`, `user_id`, `device_mac_address`, `device_name`, `device_title`, `last_update`)
-select `id`, `user_domain`, `ip`, NULL as `local_ip`, `update_token`, `user_id`, NULL as `device_mac_address`, NULL as `device_name`, NULL as `device_title`, `last_update`
+insert into `domain`
+select *
 from redirect_backup.`domain`;
 
-insert into `service` (`id`, `name`, `protocol`, `type`, `url`, `port`, `local_port`, `domain_id`)
-select `id`, `name`, `protocol`, `type`, `url`, `port`, 0 as `local_port`, `domain_id`
+insert into `service`
+select *
 from redirect_backup.`service`;
 
 insert into `action`
