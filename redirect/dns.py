2e6f01a0437236600c8c3e574eaa1d5d4e502736
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-03-21 00:02:54 +0300

    Adding SPF record

--- redirect/dns.py
+++ redirect/dns.py
@@ -13,6 +13,11 @@ class Dns:
         change = changes.add_change(change_action, full_domain, 'A')
         change.add_value(ip)
 
+    def spf_change(self, changes, ip, full_domain, change_action):
+        change = changes.add_change(change_action, full_domain, 'SPF')
+        spf_value = "v=spf1 ip4:{0}-all".format(ip)
+        change.add_value(spf_value)
+
     def mx_change(self, changes, full_domain, change_action):
         change = changes.add_change(change_action, full_domain, 'MX')
         change.add_value('1 {0}'.format(full_domain))
@@ -27,6 +32,7 @@ class Dns:
         self.a_change(changes, ip, full_domain, 'UPSERT')
         self.a_change(changes, ip, '*.{0}'.format(full_domain), 'UPSERT')
         self.mx_change(changes, full_domain, 'UPSERT')
+        self.spf_change(changes, full_domain, 'UPSERT')
 
         changes.commit()
 
@@ -37,9 +43,14 @@ class Dns:
         full_domain = domain.dns_name(main_domain)
         if zone.find_records(full_domain, 'A'):
             zone.delete_a(full_domain)
-        if zone.find_records(full_domain, 'MX'):
-            zone.delete_mx(full_domain)
-
         wildcard_domain = domain.dns_wildcard_name(main_domain)
         if zone.find_records(wildcard_domain, 'A'):
             zone.delete_a(wildcard_domain)
+
+        if zone.find_records(full_domain, 'MX'):
+            zone.delete_mx(full_domain)
+
+
+        if zone.find_records(full_domain, 'SPF'):
+            record = self.find_records(full_domain, 'SPF', identifier=None, all=False)
+            self.delete_record(record)
