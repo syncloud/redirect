572247cf6161fb1d679df07446300d1d33b1a660
Boris Rybalkin <ribalkin@gmail.com>
2017-07-23 12:52:42 +0100

    simple delete

--- redirect/dns.py
+++ redirect/dns.py
@@ -38,14 +38,14 @@ class Dns:
 
     def delete_domain(self, main_domain, domain):
         conn = boto.connect_route53(self.aws_access_key_id, self.aws_secret_access_key)
-        zone = conn.get_zone(main_domain)
+        changes = ResourceRecordSets(conn, self.hosted_zone_id)
 
+        ip = domain.dns_ip()
         full_domain = domain.dns_name(main_domain)
-        zone.delete_a(full_domain)
-        wildcard_domain = domain.dns_wildcard_name(main_domain)
-        zone.delete_a(wildcard_domain)
 
-        zone.delete_mx(full_domain)
+        self.a_change(changes, ip, full_domain, 'DELETE')
+        self.a_change(changes, ip, '*.{0}'.format(full_domain), 'DELETE')
+        self.mx_change(changes, full_domain, 'DELETE')
+        self.spf_change(changes, ip, full_domain, 'DELETE')
 
-        record = zone.find_records(full_domain, 'SPF', identifier=None, all=False)
-        zone.delete_record(record)
+        changes.commit()
