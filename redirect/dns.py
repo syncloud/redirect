d15052a4bcd3a434b2d2263e408d7264851bcd5a
Boris Rybalkin <ribalkin@gmail.com>
2017-08-10 23:27:23 +0100

    fix SPF IPv6

--- redirect/dns.py
+++ redirect/dns.py
@@ -10,17 +10,17 @@ class Dns:
         self.aws_secret_access_key = aws_secret_access_key
         self.hosted_zone_id = hosted_zone_id
 
-    def a_change(self, changes, ip, full_domain, change_action):
+    def a_change(self, changes, ip, full_domain, change_action, ip_version):
         change_type = 'A'
-        if IP(ip).version() == 6:
+        if ip_version == 6:
             change_type = 'AAAA'
 
         change = changes.add_change(change_action, full_domain, change_type)
         change.add_value(ip)
 
-    def spf_change(self, changes, ip, full_domain, change_action):
+    def spf_change(self, changes, ip, full_domain, change_action, ip_version):
         change = changes.add_change(change_action, full_domain, 'SPF')
-        spf_value = '"v=spf1 ip4:{0}-all"'.format(ip)
+        spf_value = '"v=spf1 ip{0}:{1}-all"'.format(ip_version, ip)
         change.add_value(spf_value)
 
     def mx_change(self, changes, full_domain, change_action):
@@ -39,10 +39,10 @@ class Dns:
 
         ip = domain.dns_ip()
         full_domain = domain.dns_name(main_domain)
-
-        self.a_change(changes, ip, full_domain, action)
-        self.a_change(changes, ip, '*.{0}'.format(full_domain), action)
+        ip_version = IP(ip).version()
+        self.a_change(changes, ip, full_domain, action, ip_version)
+        self.a_change(changes, ip, '*.{0}'.format(full_domain), action, ip_version)
         self.mx_change(changes, full_domain, action)
-        self.spf_change(changes, ip, full_domain, action)
+        self.spf_change(changes, ip, full_domain, action, ip_version)
 
         changes.commit()
