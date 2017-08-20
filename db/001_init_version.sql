6fd806217f54d9a1266c8ae12e58890e1aa5b4b0
Boris Rybalkin <ribalkin@gmail.com>
2017-08-20 16:20:11 +0100

    increase ip and local_ip to hold ipv6

--- db/001_init_version.sql
+++
@@ -1,6 +0,0 @@
-create table db_version (
-    `version` varchar(10) not null,
-    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-insert into db_version (version) values ('001');
--- db/006.sql
+++
@@ -1,4 +0,0 @@
-insert into `user` select * from redirect_backup.`user`;
-insert into `domain` select * from redirect_backup.`domain`;
-insert into `service` select * from redirect_backup.`service`;
-insert into `action` select * from redirect_backup.`action`;
