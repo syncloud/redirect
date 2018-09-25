a2994c87bacbe40ca60b94b0459608e57e249c06
Boris Rybalkin <ribalkin@gmail.com>
2018-09-25 23:52:47 +0100

    automate mysql

--- test.deps.sh
+++ test.deps.sh
@@ -3,6 +3,8 @@
 apt-get update -qq
 debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 apt-get install -y -qq mysql-server postfix
 pip install -r requirements.txt
 pip install -r dev_requirements.txt
