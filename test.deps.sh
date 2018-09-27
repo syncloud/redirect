09689e3800af871fa1082b309e5a235392c5d98b
Boris Rybalkin <ribalkin@gmail.com>
2018-09-27 17:56:38 +0100

    create outbox dir

--- test.deps.sh
+++ test.deps.sh
@@ -9,4 +9,5 @@ apt-get install -y -qq mysql-server libmysqlclient-dev postfix
 pip install -r requirements.txt
 pip install -r dev_requirements.txt
 adduser --disabled-password --gecos "" test
+mkdir outbox
 chown test. outbox
