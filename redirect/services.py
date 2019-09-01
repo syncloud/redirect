5553e862eb8e501a92ba877883bbb8bcf25315db
Boris Rybalkin <ribalkin@gmail.com>
2019-09-01 20:11:16 +0100

    update dns if ipv6 changed

--- redirect/services.py
+++ redirect/services.py
@@ -196,7 +196,7 @@ class Users(UsersRead):
             if not domain or not domain.user.active:
                 raise servicesexceptions.bad_request('Unknown domain update token')
 
-            update_ip = (domain.map_local_address != map_local_address) or (domain.ip != ip) or (domain.local_ip != local_ip)
+            update_ip = (domain.map_local_address != map_local_address) or (domain.ip != ip) or (domain.local_ip != local_ip) or (domain.ipv6 != ipv6)
             domain.ip = ip
             domain.local_ip = local_ip
             domain.ipv6 = ipv6
