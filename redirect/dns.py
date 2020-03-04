459ba8ee95a0156f0627395ba9cbaedf26e161e0
Boris Rybalkin <ribalkin@gmail.com>
2020-03-04 22:16:46 +0000

    fix dns record deletes

--- redirect/dns.py
+++ redirect/dns.py
@@ -9,70 +9,70 @@ class Dns:
         self.aws_secret_access_key = aws_secret_access_key
         self.hosted_zone_id = hosted_zone_id
         self.statsd_client = statsd_client
-        
-    def a_change(self, changes, ip, full_domain, change_action):
-        change = changes.add_change(change_action, full_domain, "A")
+        self.default_ipv4 = '127.0.0.1'
+        self.default_spf = '"v=spf1 -all"'
+        self.default_ipv6 = 'fe80::'
+        self.default_mx = '1 mx'
+        self.default_dkim = 'none'
+
+    def a_change(self, changes, ip, full_domain, action):
+        change = changes.add_change(action, full_domain, "A")
         change.add_value(ip)
 
-    def aaaa_change(self, changes, ip, full_domain, change_action):
-        change = changes.add_change(change_action, full_domain, "AAAA")
+    def aaaa_change(self, changes, ip, full_domain, action):
+        change = changes.add_change(action, full_domain, "AAAA")
         change.add_value(ip)
 
-    def spf_change(self, changes, full_domain, change_action):
-        spf_value = '"v=spf1 a mx -all"'
+    def spf_change(self, changes, full_domain, spf, type, action):
+        change = changes.add_change(action, full_domain, type)
+        change.add_value(spf)
 
-        change = changes.add_change(change_action, full_domain, 'SPF')
-        change.add_value(spf_value)
+    def mx_change(self, changes, full_domain, mx, action):
+        change = changes.add_change(action, full_domain, 'MX')
+        change.add_value(mx)
 
-        change = changes.add_change(change_action, full_domain, 'TXT')
-        change.add_value(spf_value)
-
-    def mx_change(self, changes, full_domain, change_action):
-        change = changes.add_change(change_action, full_domain, 'MX')
-        change.add_value('1 {0}'.format(full_domain))
-
-    def dkim_change(self, changes, full_domain, change_action, dkim_key):
+    def dkim_change(self, changes, full_domain, dkim, action):
         name = 'mail._domainkey.{0}'.format(full_domain)
-        value = '"v=DKIM1; k=rsa; p={0}"'.format(dkim_key)
-        change = changes.add_change(change_action, name, 'TXT')
-        change.add_value(value)
-
+        dkim_value = '"v=DKIM1; k=rsa; p={0}"'.format(dkim)
+        change = changes.add_change(action, name, 'TXT')
+        change.add_value(dkim_value)
+        
     def update_domain(self, main_domain, domain):
-        self.__action_domain(main_domain, domain, 'UPSERT')
+        full_domain = domain.dns_name(main_domain)
+        ipv4 = domain.dns_ipv4()
+        ipv6 = domain.dns_ipv6()
+        spf = '"v=spf1 a mx -all"'
+        mx = '1 {0}'.format(full_domain)
+        dkim = domain.dkim_key
+        self.delete_domain(main_domain, domain)
+        self.__action_domain(main_domain, full_domain, ipv4, ipv6, dkim, spf, mx, 'CREATE')
 
     def delete_domain(self, main_domain, domain):
-        self.__action_domain(main_domain, domain, 'UPSERT')
-        self.__action_domain(main_domain, domain, 'DELETE')
+        full_domain = domain.dns_name(main_domain)
+        self.__action_domain(main_domain, full_domain,self.default_ipv4, self.default_ipv6, self.default_dkim, self.default_spf, self.default_mx, 'UPSERT')
+        self.__action_domain(main_domain, full_domain,self.default_ipv4, self.default_ipv6, self.default_dkim, self.default_spf, self.default_mx, 'DELETE')
 
-    def __action_domain(self, main_domain, domain, action):
-
-        if not domain.has_dns_ip():
-            return
+    def __action_domain(self, main_domain, full_domain, ipv4, ipv6, dkim, spf, mx, action):
 
         self.statsd_client.incr('dns.ip.connect')
 
         conn = boto.connect_route53(self.aws_access_key_id, self.aws_secret_access_key)
         changes = ResourceRecordSets(conn, self.hosted_zone_id)
-
-        full_domain = domain.dns_name(main_domain)
         
-        ipv6 = domain.dns_ipv6()
         if ipv6:
             self.aaaa_change(changes, ipv6, full_domain, action)
             self.aaaa_change(changes, ipv6, '*.{0}'.format(full_domain), action)
-  
-        ipv4 = domain.dns_ipv4()
         if ipv4:
             self.a_change(changes, ipv4, full_domain, action)
             self.a_change(changes, ipv4, '*.{0}'.format(full_domain), action)
-
-        dkim_key = domain.dkim_key
-        if dkim_key:
-            self.dkim_change(changes, full_domain, action, dkim_key)
-
-        self.mx_change(changes, full_domain, action)
-
-        self.spf_change(changes, full_domain, action)
+        if dkim:
+            self.dkim_change(changes, full_domain, dkim, action)
+        if mx:
+            self.mx_change(changes, full_domain, mx, action)
+        if spf:
+            self.spf_change(changes, full_domain, spf, 'SPF', action)
+            self.spf_change(changes, full_domain, spf, 'TXT', action)
         
         self.statsd_client.incr('dns.ip.commit')
         changes.commit()
+
