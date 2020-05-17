987d756dbff045b91feea56d13a76f035db8551b
Boris Rybalkin <ribalkin@gmail.com>
2020-05-17 22:27:19 +0100

    use rvm

--- build.sh
+++ build.sh
@@ -3,17 +3,23 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR} 
 
+RUBY_VERSION=2.4.1
 BUILD_DIR=${DIR}/build
 
-apt update
-apt -y install ruby ruby-dev
-gem install jekyll
-
 mkdir ${BUILD_DIR}
-cp -r redirect ${BUILD_DIR}
+
+#apt update
+#apt -y install ruby ruby-dev
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+curl -sSL https://get.rvm.io | bash -s stable --path ruby
+source ruby/scripts/rvm
+rvm install ${RUBY_VERSION}
+gem install jekyll
 cd www
 jekyll build
 cp -r _site ${BUILD_DIR}/www
-cd ..
+
+cd ${DIR}
+cp -r redirect ${BUILD_DIR}
 
 tar czf ${DIR}/redirect.tar.gz -C ${BUILD_DIR} .
