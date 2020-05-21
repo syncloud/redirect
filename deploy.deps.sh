d1ef2dc8edc77d4469097f4ce157215e4e554eca
Boris Rybalkin <ribalkin@gmail.com>
2020-05-21 12:24:07 +0100

    more deploy deps (libapache2-mod-wsgi)

--- deploy.deps.sh
+++ deploy.deps.sh
@@ -3,4 +3,4 @@
 apt-get update -qq
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-apt-get install -y -qq mysql-server libmysqlclient-dev apache2
+apt-get install -y -qq mysql-server libmysqlclient-dev apache2 python python-pip libapache2-mod-wsgi python-mysqldb python-dev
