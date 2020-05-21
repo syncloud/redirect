82544594000dc596374c22f36192775077142c6c
Boris Rybalkin <ribalkin@gmail.com>
2020-05-21 15:52:27 +0100

    fix apache log

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -17,7 +17,7 @@ TMP_DIR = '/tmp/syncloud/ui'
 @pytest.fixture(scope="session")
 def module_setup(request, ui_mode, log_dir, artifact_dir):
     def module_teardown():
-        check_output('cp /var/log/apache/error.log {0}/apache.ui.{1}.error.log'.format(log_dir, ui_mode))
+        check_output('cp /var/log/apache/error.log {0}/apache.ui.{1}.error.log'.format(log_dir, ui_mode), shell=True)
         check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
 
     request.addfinalizer(module_teardown)
