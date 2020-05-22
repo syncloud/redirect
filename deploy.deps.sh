88865afbfd7b3fbe897665e42279e22119e638d4
Boris Rybalkin <ribalkin@gmail.com>
2020-05-22 08:37:07 +0100

    fix test domain

--- deploy.deps.sh
+++ deploy.deps.sh
@@ -4,12 +4,12 @@ apt-get update -qq
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 apt-get install -y -qq mysql-server libmysqlclient-dev apache2 python python-pip libapache2-mod-wsgi python-mysqldb python-dev openssl
-mkdir -p /etc/letsencrypt/live/syncloud.it
 
 # test deps
+mkdir -p /etc/letsencrypt/live/syncloud.test
 openssl req -x509 -newkey rsa:4096 \
-  -keyout /etc/letsencrypt/live/syncloud.it/privkey.pem \
-  -out /etc/letsencrypt/live/syncloud.it/fullchain.pem \
+  -keyout /etc/letsencrypt/live/syncloud.test/privkey.pem \
+  -out /etc/letsencrypt/live/syncloud.test/fullchain.pem \
   -nodes -days 1 \
   -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=www.example.com"
 pip install -r dev_requirements.txt
