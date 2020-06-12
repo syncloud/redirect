71088f57b29e0e7534fc02c357a4d9c2ab5eb47e
Boris Rybalkin <ribalkin@gmail.com>
2020-06-12 17:35:57 +0100

    integration secrets

--- deploy.deps.sh
+++ deploy.deps.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 apt-get update -qq
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 apt-get install -y -qq mysql-server libmysqlclient-dev apache2 python python-pip libapache2-mod-wsgi python-mysqldb python-dev openssl
 
-# test deps
+# integration deps
 mkdir -p /etc/letsencrypt/live/syncloud.test
 openssl req -x509 -newkey rsa:4096 \
   -keyout /etc/letsencrypt/live/syncloud.test/privkey.pem \
@@ -13,3 +15,4 @@ openssl req -x509 -newkey rsa:4096 \
   -nodes -days 1 \
   -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=www.example.com"
 pip install -r dev_requirements.txt
+cp ${DIR}/integration/test_secret.cfg /var/www/redirect/current/redirect/secret.cfg
