5b4e619e30918f2ca49eb0842e74a50a553eea3c
Boris Rybalkin <ribalkin@gmail.com>
2020-06-13 10:49:25 +0100

    mysql client for unit tests

--- test.deps.sh
+++ test.deps.sh
@@ -2,7 +2,7 @@
 apt-get update -qq
 debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-apt-get install -y -qq libmysqlclient-dev postfix
+apt-get install -y -qq mysql-client libmysqlclient-dev postfix
 pip install -r requirements.txt
 pip install -r dev_requirements.txt
 adduser --disabled-password --gecos "" test
