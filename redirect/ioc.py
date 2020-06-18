de44b8756b7360e6100322ce23f898149d292093
Boris Rybalkin <ribalkin@gmail.com>
2020-06-18 15:45:58 +0100

    fix ioc

--- redirect/ioc.py
+++ redirect/ioc.py
@@ -8,7 +8,9 @@ import statsd
 
 class Ioc():
     def __init__(self, redirect_config = config.read_redirect_configs('/var/www/redirect')):
-        self.statsd_client = statsd.StatsClient(redirect_config.get('stats', 'server'), 8125, prefix=the_config.get('stats', 'prefix'))
+        self.statsd_client = statsd.StatsClient(
+            redirect_config.get('stats', 'server'), 8125,
+            prefix=redirect_config.get('stats', 'prefix'))
     
         from_email = redirect_config.get('mail', 'from')
         device_error_email = redirect_config.get('mail', 'device_error')
@@ -22,7 +24,7 @@ class Ioc():
         if mock_dns:
             dns = MagicMock()
         else:
-            statsd_cli = statsd_client(the_config)
+            statsd_cli = self.statsd_client
             dns = Dns(
                 redirect_config.get('aws', 'access_key_id'),
                 redirect_config.get('aws', 'secret_access_key'),
@@ -30,7 +32,7 @@ class Ioc():
                 statsd_cli)
 
         create_storage = db_helper.get_storage_creator(redirect_config)
-        smtp = mail.get_smtp(the_config)
+        smtp = mail.get_smtp(redirect_config)
 
         the_mail = mail.Mail(smtp, from_email, activate_url_template, password_url_template, device_error_email)
         self.users_manager = services.Users(create_storage, redirect_activate_by_email, the_mail, dns, redirect_domain)
