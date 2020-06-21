c9cdb3e2221aec17e74bea04ab0657cd4ff3ab2c
Boris Rybalkin <ribalkin@gmail.com>
2020-06-21 22:27:26 +0100

    fix https

--- integration/verify.py
+++ integration/verify.py
@@ -56,7 +56,9 @@ def test_index(domain):
 
 def test_user_create_special_symbols_in_password(domain):
     email = 'symbols_in_password@mail.com'
-    response = requests.post('https://www.{0}/api/user/create'.format(domain), data={'email': email, 'password': r'pass12& ^%"'})
+    response = requests.post('https://www.{0}/api/user/create'.format(domain),
+                             data={'email': email, 'password': r'pass12& ^%"'},
+                             verify=False)
     assert response.status_code == 200
     assert smtp.emails() == 0
 
@@ -69,7 +71,11 @@ def test_user_create_success(domain):
     assert response.status_code == 200, response.text
     assert smtp.emails() > 0
     activate_token = get_token(smtp.emails()[0])
-    requests.get('https://www.{0}/api/user/activate'.format(domain), query_string={'token': activate_token})
+    requests.get('https://www.{0}/api/user/activate'.format(domain),
+                 query_string={'token': activate_token},
+                 verify=False)
     smtp.clear()
-    response = requests.get('https://www.{0}/api/user/get'.format(domain), query_string={'email': email, 'password': password})
+    response = requests.get('https://www.{0}/api/user/get'.format(domain),
+                            query_string={'email': email, 'password': password},
+                            verify=False)
     assert response.status_code == 200, response.text
