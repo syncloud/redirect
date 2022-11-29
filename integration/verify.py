cfaf5393ab9c2191b3dbf8f6384428a77f892fa8
Boris Rybalkin <ribalkin@gmail.com>
2022-11-29 08:58:14 +0000

    default-mysql-client

--- integration/verify.py
+++ integration/verify.py
@@ -51,7 +51,7 @@ def test_start(module_setup, device, device_host, domain, build_number):
     device.run_ssh("snap remove platform")
     device.run_ssh("apt-get update --allow-releaseinfo-change")
     device.run_ssh(
-        "apt-get install -y mysql-client default-libmysqlclient-dev apache2 libapache2-mod-wsgi openssl > {0}/apt.log".format(
+        "apt-get install -y default-mysql-client default-libmysqlclient-dev apache2 libapache2-mod-wsgi openssl > {0}/apt.log".format(
             TMP_DIR))
     device.scp_to_device("fakecertificate.sh", "/")
     device.run_ssh("/fakecertificate.sh")
