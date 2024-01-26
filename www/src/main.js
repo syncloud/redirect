ad2cbf0438c3e888af30556d0b4d420d64f84d02
Boris Rybalkin <ribalkin@gmail.com>
2024-01-26 22:19:08 +0000

    restore jquery

--- www/src/main.js
+++ www/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { mock } from './stub/api'
-
+import './jQuery'
+import 'bootstrap'
 if (import.meta.env.DEV) {
   mock()
 }
